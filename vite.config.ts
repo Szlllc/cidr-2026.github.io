364760446eb35a6478161ec39c7c1579a0a29b0c
林晨 <linchen@dameng.com>
2026-03-02 17:39:09 +0800

    更新

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     // 关键修改：添加 base 配置，确保打包后的资源路径指向你的仓库名
-    base: '/cidr-2026.github.io/', 
+    base: './',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
